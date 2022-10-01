1a3bfc259c83b71e024882d0895842f6df438884
avinashjindal786 <avinashjindal786@gmail.com>
2022-10-01 12:48:16 +0530

    Initial Commit

---
+++ uiTest/uiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiTestApp.swift
+//  uiTest
+//
+//  Created by Avinash jindal on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct uiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
